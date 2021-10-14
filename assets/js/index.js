63d2728aafd8529aeaf1a7724f0c0a4d3bf81236
dev-allan <allanduminy@protonmail.com>
2021-10-14 22:12:18 +0200

    fin projet meteo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,21 @@
 //Récupération des éléments DOM
 let searchInput = document.querySelector("#inputSearch");
-let displayResult = document.querySelector("#displayMeteo");
+let body = document.querySelector("body");
 
 //Ajout de l'attribut value afin d'obtenir la valeur dans l'input
 searchInput.setAttribute("value", "");
 
-let arrayData = [];
+//JSON permettant de traduire les data obtenu de openWeather
+let data = {
+    "Clouds" : "Nuageux",
+    "Clear" : "Dégagé",
+    "Rain" : "Pluie",
+    "Snow" : "Neige",
+    "Drizzle" : "Bruine",
+    "Thunderstorm" : "Orage"
+};
+
+
 
 /**
  * Fonction qui permet d'obtenir les données météorologiques
@@ -18,11 +28,25 @@ const askMeteo = (city) => {
         }
     })
     .then(function(value){
+        const displayR = document.createElement("section");
+        displayR.setAttribute("id", "displayMeteo");
+        body.appendChild(displayR);
+        console.log(value);
         for(let i = 0; i < value.list.length; i++){
             const div = document.createElement("div");
-            displayResult.appendChild(div);
-            div.setAttribute('id', 'card')
-            div.innerHTML = `<p>humidité = ${value.list[i].main.humidity}</p>`
+            displayR.appendChild(div);
+            div.setAttribute('class', 'card');
+            let meteo = value.list[i].weather[0].main
+            let icon = value.list[i].weather[0].icon
+            div.innerHTML =     `
+                                <div class="cardHeader">
+                                    <h3>${value.city.name}</h3>
+                                    <img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="Icone météo">
+                                </div>
+                                <p>Date : ${value.list[i].dt_txt}</p>
+                                <p>Météo : ${data[meteo]}</p>
+                                <p>Humidité : ${value.list[i].main.humidity}</p>
+                                <p>Température : ${Math.round(value.list[i].main.temp - 273.15)}°</p>`
         }
     })
     .catch(function(error){
@@ -30,14 +54,11 @@ const askMeteo = (city) => {
     })
 }
 
-console.log(arrayData);
-
 //événement qui écoute le click du bouton afin de rechercher une ville
 btnAddTask.addEventListener('click', (event) => {
-    if(document.getElementById('card') !== null){
-        document.getElementById('card').remove()
-    }
     event.preventDefault();
+    let removeBlockDisplayMeteo = document.querySelector("#displayMeteo");
+    removeBlockDisplayMeteo.remove();
     askMeteo(searchInput.value)
     event.stopPropagation();
 })
